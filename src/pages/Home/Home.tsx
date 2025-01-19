782e5faf54d7a69a7e884fde5e4418c9b58ca5a7
Petr Lipatov <lipatov3@gmail.com>
2025-01-19 18:18:31 +0300

    feat: Images data fetching from api added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
 import { CanvasScene } from "@/components/CanvasScene";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
-  return <CanvasScene />;
+  const [images, setImages] = useState(null);
+  useEffect(() => {
+    fetch("https://api.stepanplusdrawingultra.site/images")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setImages(jsonData);
+        console.log(jsonData);
+      })
+      .catch((error) => {
+        console.error("Ошибка при загрузке данных:", error);
+      });
+  }, []);
+  return images && <CanvasScene />;
 };
