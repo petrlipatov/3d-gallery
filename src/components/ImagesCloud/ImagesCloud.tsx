7df36f08a10715900a9e18bff50414cd699c18ca
Petr Lipatov <lipatov3@gmail.com>
2025-01-20 14:21:44 +0300

    refactor: Animation controller optimized

--- src/components/ImagesCloud/ImagesCloud.tsx
+++ src/components/ImagesCloud/ImagesCloud.tsx
@@ -14,6 +14,8 @@ import { useViewport } from "@/shared/hooks/useViewport";
 import { generateGridPositions } from "@/shared/helpers";
 import { imagesContext } from "@/shared/constants/contexts";
 
+const targetVector = new THREE.Vector3();
+
 export const ImagesCloud = ({
   activeAnimation,
   onClick,
@@ -42,124 +44,123 @@ export const ImagesCloud = ({
     setSelectedIndex(randomIndex);
   }, [randomCoordinates.length]);
 
-  useFrame(() => {
-    if (!isAnimating) {
-      return;
-    }
+  useFrame(function animationControllers() {
+    if (!isAnimating) return;
 
-    if (activeAnimation === "grid") {
-      refs.current.forEach((ref, i) => {
-        ref.position.lerp(
-          new THREE.Vector3(
+    switch (activeAnimation) {
+      case "grid": {
+        refs.current.forEach((ref, i) => {
+          targetVector.set(
             positionsGrid[i][0],
             positionsGrid[i][1],
             positionsGrid[i][2]
-          ),
-          0.08
-        );
-      });
-    } else if (activeAnimation === "shuffle") {
-      refs.current.forEach((ref, i) => {
-        ref.position.lerp(
-          new THREE.Vector3(
+          );
+          ref.position.lerp(targetVector, 0.08);
+        });
+        break;
+      }
+      case "shuffle": {
+        refs.current.forEach((ref, i) => {
+          targetVector.set(
             randomCoordinates[i][0],
             randomCoordinates[i][1],
             randomCoordinates[i][2]
-          ),
-          0.05
-        );
-      });
-    } else if (activeAnimation === "random") {
-      if (selectedIndex !== null) {
-        const selected = refs.current[selectedIndex];
-        if (selected) {
-          const targetPosition = camera.position
-            .clone()
-            .add(
-              camera.getWorldDirection(new THREE.Vector3()).multiplyScalar(2)
-            );
-          selected.position.lerp(targetPosition, 0.09);
-
-          refs.current.forEach((ref, i) => {
-            if (i !== selectedIndex) {
-              const targetPosition = ref.position.clone() as THREE.Vector3;
-
-              targetPosition.setZ(farAway);
-              targetPosition.add(new THREE.Vector3(0, 1, 0));
-
-              ref.position.lerp(targetPosition, 0.1);
-            }
-          });
-        }
+          );
+          ref.position.lerp(targetVector, 0.05);
+        });
+        break;
       }
-    } else if (activeAnimation === "whomi") {
-      refs.current.forEach((ref) => {
-        const targetPosition = ref.position.clone() as THREE.Vector3;
+      case "random": {
+        if (selectedIndex !== null) {
+          const selected = refs.current[selectedIndex];
+          if (selected) {
+            const targetPosition = camera.position
+              .clone()
+              .add(
+                camera.getWorldDirection(new THREE.Vector3()).multiplyScalar(2)
+              );
+            selected.position.lerp(targetPosition, 0.09);
 
-        targetPosition.setZ(farAway);
-        targetPosition.add(new THREE.Vector3(0, 1, 0));
+            refs.current.forEach((ref, i) => {
+              if (i !== selectedIndex) {
+                const targetPosition = ref.position.clone() as THREE.Vector3;
+                targetPosition.setZ(farAway);
+                targetPosition.add(new THREE.Vector3(0, 1, 0));
+                ref.position.lerp(targetPosition, 0.1);
+              }
+            });
+          }
+        }
+        break;
+      }
+      case "whomi": {
+        refs.current.forEach((ref) => {
+          const targetPosition = ref.position.clone() as THREE.Vector3;
 
-        ref.position.lerp(targetPosition, 0.1);
-      });
+          targetPosition.setZ(farAway);
+          targetPosition.add(new THREE.Vector3(0, 1, 0));
+
+          ref.position.lerp(targetPosition, 0.1);
+        });
+        break;
+      }
     }
   });
 
-  useEffect(() => {
-    if (isAnimating) {
-      return;
-    }
+  useEffect(
+    function setDragControls() {
+      if (isAnimating) return;
 
-    const dragControls = new DragControls(refs.current, camera, gl.domElement);
+      const dragControls = new DragControls(
+        refs.current,
+        camera,
+        gl.domElement
+      );
 
-    const setDraggingOnWithDelay = () => {
-      dragStartTimerRef.current = setTimeout(() => {
-        setIsDragged(true);
-        console.log("on");
-      }, 400);
-      setIsControlsEnabled(false);
-    };
+      const setDraggingOnWithDelay = () => {
+        dragStartTimerRef.current = setTimeout(() => {
+          setIsDragged(true);
+        }, 400);
+        setIsControlsEnabled(false);
+      };
 
-    const setDraggingOffWithDelay = () => {
-      dragEndTimerRef.current = setTimeout(() => {
-        setIsDragged(false);
-      }, 400);
-      setIsControlsEnabled(true);
-    };
+      const setDraggingOffWithDelay = () => {
+        dragEndTimerRef.current = setTimeout(() => {
+          setIsDragged(false);
+        }, 400);
+        setIsControlsEnabled(true);
+      };
 
-    const handleDrag = () => {
-      if (dragStartTimerRef.current) {
-        return;
-      }
-      setDraggingOnWithDelay();
-    };
+      const handleDrag = () => {
+        if (dragStartTimerRef.current) {
+          return;
+        }
+        setDraggingOnWithDelay();
+      };
 
-    const handleDragEnd = () => {
-      if (dragStartTimerRef.current) {
-        clearTimeout(dragStartTimerRef.current);
-        dragStartTimerRef.current = null;
-      }
-      if (dragEndTimerRef.current) clearTimeout(dragEndTimerRef.current);
+      const handleDragEnd = () => {
+        if (dragStartTimerRef.current) {
+          clearTimeout(dragStartTimerRef.current);
+          dragStartTimerRef.current = null;
+        }
+        if (dragEndTimerRef.current) clearTimeout(dragEndTimerRef.current);
 
-      setDraggingOffWithDelay();
-    };
+        setDraggingOffWithDelay();
+      };
 
-    dragControls.addEventListener("drag", handleDrag);
-    dragControls.addEventListener("dragend", handleDragEnd);
+      dragControls.addEventListener("drag", handleDrag);
+      dragControls.addEventListener("dragend", handleDragEnd);
 
-    return () => {
-      dragControls.removeEventListener("drag", handleDrag);
-      dragControls.removeEventListener("dragend", handleDragEnd);
-
-      if (dragStartTimerRef.current) {
-        clearTimeout(dragStartTimerRef.current);
-      }
-      if (dragEndTimerRef.current) {
-        clearTimeout(dragEndTimerRef.current);
-      }
-
-      dragControls.dispose();
-    };
-  }, [camera, gl, setIsControlsEnabled, isAnimating, setIsDragged]);
+      return () => {
+        dragControls.removeEventListener("drag", handleDrag);
+        dragControls.removeEventListener("dragend", handleDragEnd);
+        if (dragStartTimerRef.current) clearTimeout(dragStartTimerRef.current);
+        if (dragEndTimerRef.current) clearTimeout(dragEndTimerRef.current);
+        dragControls.dispose();
+      };
+    },
+    [camera, gl, setIsControlsEnabled, isAnimating, setIsDragged]
+  );
 
   useEffect(() => {
     if (activeAnimation === "random") {
