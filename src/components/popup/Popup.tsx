06d2fb640008592bea460b0c38153bd8b020406d
Petr Lipatov <lipatov3@gmail.com>
2025-02-20 23:53:26 +0300

    fix: images context switched for imageStore in Popup

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { createPortal } from "react-dom";
 import { LazyImage } from "@/shared/ui/lazy-image";
-import { imagesContext } from "@/shared/constants/contexts";
+import { storeContext } from "@/shared/constants/contexts";
 import { BASE_API_URL, IMAGES_PATH } from "@/shared/constants";
 import s from "./Popup.module.css";
 
 export function Popup({ image, onClose }) {
-  const imagesData = useContext(imagesContext);
-  const imageData = imagesData[image];
+  const { imagesStore } = useContext(storeContext);
+  const imageData = imagesStore.images[image];
 
   return createPortal(
     <div className={s.popup} onClick={onClose}>
