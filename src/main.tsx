f76ef5e5b3e342e62fdd3bb4096f30009833fc14
Petr Lipatov <lipatov3@gmail.com>
2025-03-30 16:32:52 +0300

    GA4 added

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./components/app";
+import ReactGA from "react-ga4";
 import "./index.css";
+
+ReactGA.initialize("G-SZZ6EBZJD3");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
